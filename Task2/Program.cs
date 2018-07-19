bac8ed4931f7846fe62bd2207db38dd2620d80b0
Aleksandr Konovaltcev <akonovalcev@gmail.com>
2018-07-19 15:02:47 +0400

    Completed Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // Дана коллекция List<T>​. Требуется подсчитать, сколько раз каждый элемент встречается в
 // данной коллекции:
@@ -18,7 +16,17 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            var listInt = new List<int> { 1, 2, 3, 4, 5, 2, 3, 4, 6, 7, 8, 9, 8, 9, 10 };
+            var listString = new List<string> { "hello", "hi", "hello", "hi", "hello" };
+            GetCountOfEveryElementInGenericCollection(listInt);
+            GetCountOfEveryElementInGenericCollection(listString);
 
+        }
+
+        private static void GetCountOfEveryElementInGenericCollection<T>(List<T> list)
+        {
+            var result = list.GroupBy(i => i);
+            foreach (var i in result) Console.WriteLine($@"element {i.Key} is repeated {i.Count()} times");
         }
     }
 }
